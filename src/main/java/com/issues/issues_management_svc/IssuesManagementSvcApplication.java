fe0ab8786fbbb7099f46f7eee112e9132bbf9612
USER <1905707@kiit.ac.in>
2022-10-22 21:37:05 +0530

    Initial commit...

---
+++ src/main/java/com/issues/issues_management_svc/IssuesManagementSvcApplication.java
@@ -0,0 +1,13 @@
+package com.issues.issues_management_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesManagementSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesManagementSvcApplication.class, args);
+	}
+
+}
